acdd9453ccecb848f448d33dc4b86c21c89344ef
annalhq <annalhqshaikh@gmail.com>
2025-10-05 00:03:15 +0530

    feat: token debug

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 
 #define d_vocab 5027
 
@@ -16,8 +17,22 @@ struct decoder {
 };
 
 int main(void) {
+  size_t offset = 0;
+  size_t decoder_offset = offset;
+  offset += sizeof(struct decoder);
+
+  char *raw_mem = malloc(offset);
+  struct decoder *decoder = (struct decoder *)raw_mem + decoder_offset;
+
   FILE *f = fopen("enc", "r");
   assert(f);
+
+  unsigned long read = fread(decoder, 1, sizeof(struct decoder), f);
+  assert(read == sizeof(struct decoder));
+
+  printf("offset: %us, size: %u\n", decoder->tokens[0].offset,
+         decoder->tokens[0].size);
+
   fclose(f);
   printf("test\n");
 }
